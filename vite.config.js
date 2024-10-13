0b2e4f6d2b19cb379302f68d6a32ac6a19e4989c
Dikens27 <kar.karovich321@gmail.com>
2024-10-14 00:04:49 +0300

    fix-maby

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/Klinika-2/',
     build: {
       sourcemap: true,
       rollupOptions: {
